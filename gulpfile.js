f22d52d06c370dd65bce76c294acfd3f00b1af62
Gustavo <gustavo@bastly.com>
2015-09-12 14:45:53 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var reload      = browserSync.reload;
+
+var src = {
+    scss: 'app/scss/*.scss',
+    css:  'app/css',
+    html: 'app/*.html'
+};
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync({
+        server: "./app",
+        host: "192.168.1.45"
+    });
+
+    gulp.watch(src.scss, ['sass']);
+    gulp.watch(src.html).on('change', reload);
+});
+
+// Compile sass into CSS
+gulp.task('sass', function() {
+    return gulp.src(src.scss)
+        .pipe(sass())
+        .pipe(gulp.dest(src.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('default', ['serve']);
